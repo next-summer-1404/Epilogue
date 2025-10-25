340c9ae076e6c4fffee142fd5aad0052bc473cb8
TinaMahmoudi1 <tinamahmoudi201@gmail.com>
2025-10-25 14:37:44 -0800

    Responsive

--- src/app/(main)/contact-us/page.tsx
+++ src/app/(main)/contact-us/page.tsx
@@ -6,18 +6,24 @@ import ContactUsForm from './components/Form/ContactUsForm'
 
 const page = () => {
     return (
-        <div className='pb-[156px] px-4 sm:px-8'>
+        <div className='pb-[156px] px-4 sm:px-8 lg:px-12'>
             <div className="flex flex-col gap-6 pb-4">
                 <BreadcrumbsFile />
                 <p className='text-[#8CFF45]'>ارتباط با ما </p>
 
                 <div className='flex flex-col lg:flex-row gap-12 pt-[80px]'>
+                    {/* بخش سمت راست */}
                     <div className='flex-1 flex justify-center lg:justify-start'>
-                        <RightSection />
+                        <div className="w-full max-w-md">  {/* محدودیت عرض */}
+                            <RightSection />
+                        </div>
                     </div>
 
+                    {/* فرم */}
                     <div className='flex-1 flex justify-center lg:justify-end mt-8 lg:mt-0'>
-                        <ContactUsForm />
+                        <div className="w-full max-w-lg"> {/* محدودیت عرض فرم */}
+                            <ContactUsForm />
+                        </div>
                     </div>
                 </div>
             </div>
